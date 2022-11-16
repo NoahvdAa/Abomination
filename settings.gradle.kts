8832ef4be21f8ae2a3f13116b87c4c20a370c1d4
Noah van der Aa <ndvdaa@gmail.com>
2022-11-16 16:59:12 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://papermc.io/repo/repository/maven-public/")
+    }
+}
+
+rootProject.name = "abomination"
+
+include("abomination-api", "abomination-server")
